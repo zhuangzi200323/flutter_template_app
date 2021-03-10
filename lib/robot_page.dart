1bda209ba7fa9b08ef7ac4e5373975cba60f82b6
zhuangzi200323 <zhuangzi200323@163.com>
2021-03-10 16:08:33 +0800

    加入login页面

--- lib/robot_page.dart
+++ lib/robot_page.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_template_app/study/LoginPage.dart';
 
 class RobotPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Center(
-          child: Text(AppLocalizations.of(context).robot),
-        ),
+        body: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  child: Text("LoginPage"),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder:(context) {
+                      return LoginPage();
+                    }));
+                  },
+                ),
+              ]
+          ),
+      ),
     );
   }
 }
